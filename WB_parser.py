bb2e73f84ee9421f2839e55f85a875c1b4c978d1
max31ru12 <maxevg72@gmail.com>
2022-06-23 23:50:05 +0300

    added first page data and hrefs_file

--- WB_parser.py
+++ WB_parser.py
@@ -2,11 +2,12 @@
 import requests
 from datetime import datetime
 import json
-from bs4 import BeautifulSoup
-import bs4
 from selenium import webdriver # Это типа импорт селениума
 from selenium.webdriver.common.by import By
 import pandas as pd
+import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # щас импортируем работу селениума с выпадающими списками
 from selenium.webdriver.support.select import Select
@@ -50,6 +51,9 @@ json_urls_2 = [
 name_list = []
 brand_list = []
 
+
+
+
 def collect_data(url, n):
     # Получаю ответ на запрос
     responce = requests.get(url=url)
@@ -66,8 +70,10 @@ def collect_data(url, n):
         brand = one_name['brand']
         name_list.append(name)
         brand_list.append(brand)
-        # print(f'{name}, {brand}')
     return None
+
+
+
 
 def collect_data_two(url, n):
     responce = requests.get(url)
@@ -83,62 +89,105 @@ def collect_data_two(url, n):
         brand_list.append(brand_name['name'])
 
 
-collect_data_two(json_urls_2[3], 16)
 
-def selenium_parse():
-    pass
+
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ru,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.160 YaBrowser/22.5.2.615 Yowser/2.5 Safari/537.36'
+}
+
+
+firm_name = []
+href_list = []
+info_list = []
+firm_name_done = []
+
+
+
+
+def parse_page_one():
+    driver = webdriver.Chrome()
+    driver.get(url = page_urls[0])
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="c81887951"]'))
+    )
+    el = driver.find_element(By.XPATH, '//*[@id="catalog-content"]/div/div').find_elements(By.CLASS_NAME, 'product-card.j-card-item.j-good-for-listing-event')
+
+    for element in el:
+        info = element.find_element(By.CLASS_NAME, 'product-card__wrapper')
+        a_tag = element.find_element(By.CLASS_NAME, 'product-card__wrapper').find_element(By.TAG_NAME, 'a')
+        href = a_tag.get_attribute('href')
+        href_list.append(href)
+        info_list.append(info.text)
+
+    el_two = driver.find_element(By.XPATH, '//*[@id="catalog-content"]/div/div').find_elements(By.CLASS_NAME, 'product-card.j-card-item')
+    for element in el_two:
+        info_two = element.find_element(By.CLASS_NAME, 'product-card__wrapper')
+        a_tag_two = element.find_element(By.CLASS_NAME, 'product-card__wrapper').find_element(By.TAG_NAME, 'a')
+        href_two = a_tag_two.get_attribute('href')
+        href_list.append(href_two)
+        info_list.append(info_two.text)
+    date = datetime.now().strftime('%d_%m_%Y')
+
+    pattern = r'(\w+\s)?\w+\s/'
+    i = 1
+
+
+    for el in info_list:
+        result = re.search(pattern, el)
+        if result == None:
+            result = 'Мистраль..'
+            firm_name.append(result)
+            continue
+        firm_name.append(result[0])
+
+    for name in firm_name:
+        name = re.sub(r'\s/', '', name)
+        firm_name_done.append(name)
+
+    with open(f'hrefs_{date}.txt', 'w', encoding='utf-8') as f:
+        for el in href_list:
+            f.write(f'{el}\n')
+    if date == '23_06_2022':
+        selenium_table = pd.DataFrame({'name': 'Хлопья овсяные',
+                              'brand name': firm_name_done,
+                              'href': href_list
+                                })
+        selenium_table.to_excel(f'another_table{date}.xlsx')
+
 
 
 
 def main():
     count = 1
     count_two = 4
+    now_date = datetime.now().strftime('%d_%m_%Y')
+    if now_date == '21_06_2022':
+        for json_page in json_urls:
+            collect_data(json_page, count)
+            count = count + 1
 
-    for json_page in json_urls:
-        collect_data(json_page, count)
-        count = count + 1
+        for json_page in json_urls_2:
+            collect_data_two(json_page, count_two)
+            count_two = count_two + 1
 
-    for json_page in json_urls_2:
-        collect_data_two(json_page, count_two)
-        count_two = count_two + 1
+    parse_page_one()
 
-    selenium_parse()
+
+
 
 if __name__ == '__main__':
     main()
 
-# Так можно открыть хром с помощью selenium
-# Слэши эуранируются (то есть два обратных слеша)
-# browser = webdriver.Chrome(executable_path='C:\\Users\\Максим\\Desktop\\WB_parser\\chromedriver.exe')
-# browser.maximize_window() # Сразу открывается на весь экран
-# # Получаем какую-то ссылку
-# browser.get(url)
-# # Поиск элемента
-# href = browser.find_element(By.XPATH, value='')
-# print(f'{href.text}, хуй')
+# table = pd.DataFrame({'name': name_list,
+#          'brand name': brand_list})
+#
+# date = datetime.now().strftime('%d_%m_%Y')
+#
+# table.to_excel(f'table{date}.xlsx')
 
-driver = webdriver.Chrome()
 
-try:
-    driver.get(url=page_urls[0])
-    # driver.maximize_window()
-    # time.sleep(2) # Чтобы браузер сразу не закрылся
-    frame = driver.find_element(By.XPATH, '//*[@id="c12117076"]/div/a')
-    # with open('selenium_file.txt', 'w') as f:
-    #     f.write(page)
-except Exception as ex:
-    print('Пошел нахуй')
-finally:
-    driver.close()
-    driver.quit()
-
-# print(name_list.__sizeof__())
-# print(brand_list.__sizeof__())
-
-table = pd.DataFrame({'name': name_list,
-         'brand name': brand_list})
-
-date = datetime.now().strftime('%d_%m_%Y')
-
-table.to_excel(f'table{date}.xlsx')
 
